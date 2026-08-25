0c42ecf7f0974dc39c4812f32b9b96bb25faa406
Swarnava Banerjee <banerjeeswarnava914@gmail.com>
2026-08-26 00:13:43 +0530

    Alembic ready for core support tables

--- migrations/env.py
+++ migrations/env.py
@@ -14,11 +14,19 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from packages.config.settings import get_settings
+from packages.database.base import Base
+
+import packages.database.models
+
+settings = get_settings()
+
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.database_url,
+)
+
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
---
+++ packages/database/models/__init__.py
@@ -0,0 +1,9 @@
+from packages.database.models.support.user import UserModel
+from packages.database.models.support.conversation import ConversationModel
+from packages.database.models.support.message import MessageModel
+
+__all__ = [
+    "UserModel",
+    "ConversationModel",
+    "MessageModel",
+]
